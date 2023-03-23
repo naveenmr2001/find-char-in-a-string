775b26912658323b3d09d53f2f79004652ef8e0a
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-23 15:47:44 +0530

    Add. I have added the logic to find the char in the string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String to Search: ");
+
+        String stringToSearch = scanner.nextLine();
+
+        System.out.println("Enter the Char to Search: ");
+
+        char charToSearch = scanner.next().charAt(0);
+
+        boolean flagToSearch = true;
+
+        for(int iterator = 0 ; iterator<stringToSearch.length() ; iterator++ ){
+
+            if(stringToSearch.charAt(iterator) == charToSearch){
+
+                System.out.println("Yes the char is present in the string");
+
+                flagToSearch = false;
+
+                break;
+
+            }
+
+        }
+
+        if(flagToSearch){
+            System.out.println("No the char is not present in the string");
+        }
+
     }
 }
